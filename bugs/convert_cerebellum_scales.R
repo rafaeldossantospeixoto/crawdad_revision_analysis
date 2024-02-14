9070f55618416734cc506c39cb65ea9cda9e34fe
Rafael dos Santos Peixoto <rafaeldossantospeixoto@gmail.com>
2024-02-14 16:35:11 -0500

    attempt to fix bugs in scale

--- bugs/convert_cerebellum_scales.R
+++ bugs/convert_cerebellum_scales.R
@@ -1,11 +1,48 @@
 library(crawdad)
 library(tidyverse)
 
+file1 <- readRDS('bugs/slideseq_cerebellum/cerebellum/F_GRCm38.81.P60Cerebellum_ALT.filtered.raw.dge.RDS')
+dim(file1)
+file1[1:7,1:7]
+
+file1 <- readRDS('bugs/slideseq_cerebellum/cerebellum/F_GRCm38.81.P60Cerebellum_ALT.cell_cluster_outcomes.RDS')
+dim(file1)
+head(file1)
+
+file1 <- read.csv('bugs/slideseq_cerebellum/Puck_180819_11/BeadMapping_10-17_0913/BeadLocationsForR.csv')
+dim(file1)
+head(file1)
+max(file1$xcoord) - min(file1$xcoord) # 3886.176
+max(file1$ycoord) - min(file1$ycoord) # 4366.586
+file1 %>% 
+  ggplot() +
+  geom_point(aes(x=xcoord, y=ycoord), size = .1)
+
+file1 <- read.csv('bugs/slideseq_cerebellum/Puck_180819_11/BeadMapping_10-17_0913/BeadLocationsForRUncropped.csv')
+dim(file1)
+head(file1)
+max(file1$xcoord) - min(file1$xcoord) # 4203.508
+max(file1$ycoord) - min(file1$ycoord) # 4366.586
+file1 %>% 
+  ggplot() +
+  geom_point(aes(x=xcoord, y=ycoord), size = .1)
+
+file1 <- read.csv('bugs/slideseq_cerebellum/Puck_180819_12/BeadLocationsForR.csv')
+dim(file1)
+head(file1)
+max(file1$xcoord) - min(file1$xcoord) # 5798.119
+max(file1$ycoord) - min(file1$ycoord) # 5616.116
+file1 %>% 
+  ggplot() +
+  geom_point(aes(x=xcoord, y=ycoord), size = .1)
+
+setwd("~/Library/CloudStorage/OneDrive-JohnsHopkins/jefworks/crawdad/repos/crawdad_revision_analysis")
+setwd("/Users/rafaeldossantospeixoto/Library/CloudStorage/OneDrive-JohnsHopkins/jefworks/crawdad/repos/CRAWDAD")
 
 # slideSeq ----------------------------------------------------------------
 
 data('slide')
 max(slide$x) - min(slide$x) # 4628.55
 max(slide$y) - min(slide$y) # 3800.465
-## it should be aprox 2904 and 2425 microns, not 4628.55 or 3800.465
+## it should be aprox 2904 and 2425 microns, not 4628.55 and 3800.465
 ggplot(slide, aes(x, -y)) + geom_point(size = .1)
--- bugs/convert_embryo_scales.R
+++ bugs/convert_embryo_scales.R
@@ -1,6 +1,7 @@
 library(crawdad)
 library(tidyverse)
 
+setwd("~/Library/CloudStorage/OneDrive-JohnsHopkins/jefworks/crawdad/repos/crawdad_revision_analysis")
 
 # seqFISH ----------------------------------------------------------------
 
@@ -9,3 +10,14 @@ max(seq$x) - min(seq$x) # 5069.662
 max(seq$y) - min(seq$y) # 6984.168
 ## it should be aprox 1000 microns, not 5000 or 6000
 ggplot(seq, aes(x, -y)) + geom_point(size = .1)
+
+meta_seq <- readRDS('bugs/seqfish_embryo/metadata.Rds')
+head(meta_seq)
+table(meta_seq$embryo)
+meta_seq <- meta_seq %>% 
+  filter(embryo == "embryo1")
+names(meta_seq)
+max(meta_seq$x_global) - min(meta_seq$x_global) # 157.1595
+max(meta_seq$y_global) - min(meta_seq$y_global) # 216.5092
+max(meta_seq$x_global_affine) - min(meta_seq$x_global_affine) # 5.069662
+max(meta_seq$y_global_affine) - min(meta_seq$y_global_affine) # 6.984168
