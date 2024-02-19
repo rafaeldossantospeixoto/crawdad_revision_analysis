9c48fb0c0b29feed19d933c76368b098bbb167ed
Rafael dos Santos Peixoto <rafaeldossantospeixoto@gmail.com>
2024-02-19 13:26:49 -0500

    run CRAWDAD and Ripley's on embryo

---
+++ running_code/ripleys/embryo_ripleys.R
@@ -0,0 +1,86 @@
+library(crawdad)
+library(tidyverse)
+library(spatstat)
+
+data(seq)
+
+
+# Endothelium -------------------------------------------------------------
+
+reference_ct <- 'Endothelium'
+
+## run ripley's L
+## need to convert to these point process objects
+ppo  <- ppp(x=seq$x, y=seq$y, 
+            window = owin(range(seq$x), range(seq$y)), 
+            marks=factor(seq$celltypes))
+## run on all
+rkc <- do.call(rbind, lapply(levels(ppo$marks), function(x) {
+  test <- Kcross(ppo,reference_ct, x)
+  ## isotropic-corrected minus theoretical
+  test$iso - test$theo
+}))
+rownames(rkc) <- levels(ppo$marks)
+# colnames(rkc) <- ppo$r
+
+## plot
+rkc.melt <- reshape2::melt(rkc)
+colnames(rkc.melt) <- c('celltype', 'distance', 'score')
+head(rkc.melt)
+
+rkc.melt %>%  
+  ggplot() +
+  geom_line(aes(x=distance, y=score, color=celltype)) +
+  ggplot2::geom_hline(yintercept = 0, color = "black", size = 0.6, linetype = "dotted") +
+  ggplot2::theme_classic() +
+  ggplot2::scale_color_manual(values = rainbow(length(unique(rkc.melt$celltype)))) +
+  labs(y = "isotropic-corrected minus theoretical score")
+
+df <- rkc.melt
+colnames(df) <- c('neighbor', 'distance', 'score')
+df$reference <- reference_ct
+df$permutation <- 1
+saveRDS(df, paste0("running_code/processed_data/dat_embryo_ripleys_",
+                   sub(" ", "_", reference_ct),
+                   ".RDS"))
+
+
+
+# Endothelium -------------------------------------------------------------
+
+reference_ct <- 'Intermediate mesoderm'
+
+## run ripley's L
+## need to convert to these point process objects
+ppo  <- ppp(x=seq$x, y=seq$y, 
+            window = owin(range(seq$x), range(seq$y)), 
+            marks=factor(seq$celltypes))
+## run on all
+rkc <- do.call(rbind, lapply(levels(ppo$marks), function(x) {
+  test <- Kcross(ppo,reference_ct, x)
+  ## isotropic-corrected minus theoretical
+  test$iso - test$theo
+}))
+rownames(rkc) <- levels(ppo$marks)
+# colnames(rkc) <- ppo$r
+
+## plot
+rkc.melt <- reshape2::melt(rkc)
+colnames(rkc.melt) <- c('celltype', 'distance', 'score')
+head(rkc.melt)
+
+rkc.melt %>%  
+  ggplot() +
+  geom_line(aes(x=distance, y=score, color=celltype)) +
+  ggplot2::geom_hline(yintercept = 0, color = "black", size = 0.6, linetype = "dotted") +
+  ggplot2::theme_classic() +
+  ggplot2::scale_color_manual(values = rainbow(length(unique(rkc.melt$celltype)))) +
+  labs(y = "isotropic-corrected minus theoretical score")
+
+df <- rkc.melt
+colnames(df) <- c('neighbor', 'distance', 'score')
+df$reference <- reference_ct
+df$permutation <- 1
+saveRDS(df, paste0("running_code/processed_data/dat_embryo_ripleys_",
+                   sub(" ", "_", reference_ct),
+                   ".RDS"))
---
+++ running_code/run_embryo.R
@@ -0,0 +1,31 @@
+# Run sim samples ---------------------------------------------------------
+
+library(crawdad)
+library(tidyverse)
+ncores <- 7
+
+data(seq)
+
+## convert to sf
+seq <- crawdad:::toSF(pos = seq[,c("x", "y")],
+                        celltypes = seq$celltypes)
+
+scales <- seq(100, 1000, by=100)
+
+## generate background
+shuffle.list <- crawdad:::makeShuffledCells(seq,
+                                            scales = scales,
+                                            perms = 3,
+                                            ncores = ncores,
+                                            seed = 1,
+                                            verbose = TRUE)
+
+## find trends, dist 50
+results_50 <- crawdad::findTrends(seq,
+                                  dist = 50,
+                                  shuffle.list = shuffle.list,
+                                  ncores = ncores,
+                                  verbose = TRUE,
+                                  returnMeans = FALSE)
+dat_50 <- crawdad::meltResultsList(results_50, withPerms = T)
+saveRDS(dat_50, 'running_code/processed_data/dat_seq_50.RDS')
