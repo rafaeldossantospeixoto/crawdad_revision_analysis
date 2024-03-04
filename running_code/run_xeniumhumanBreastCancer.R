03e5a885724de2d8e3e6e7f99cffcaacfcfbd46a
Gohta Aihara <gohta.aihara@gmail.com>
2024-03-04 17:52:27 -0500

    update

--- running_code/run_xeniumhumanBreastCancer.R
+++ running_code/run_xeniumhumanBreastCancer.R
@@ -65,14 +65,16 @@ col_ct <- gg_color_hue(length(levels(ct_labels)))
 names(col_ct) <- levels(ct_labels)
 
 ## Figure x (singecell visualization)
+# ggplot
 df <- data.frame(spatialCoords(spe), celltype = colData(spe)$celltype)
 ggplot(df, aes(x = x, y = y, col = celltype)) +
-  rasterise(geom_point(size = 0.5, stroke = 0.01), dpi = 300) +
+  coord_fixed() +
+  rasterise(geom_point(size = 0.3, stroke = 0.01), dpi = 300) +
   scale_color_manual(values = col_ct) +
   guides(col = guide_legend(override.aes = list(size = 3))) +
   labs(x = "x (um)",
        y = "y (um)",
-       col = "Cell types") +
+       col = "cell types") +
   theme_bw() +
   theme(
     panel.grid = element_blank(),
@@ -98,20 +100,29 @@ ggsave(filename = here("running_code", "plots", dataset_name, paste0(dataset_nam
 
 ## remove unlabeled
 dat_filtered <- dat[!(dat$neighbor == "Unlabeled" | dat$reference == "Unlabeled"),]
-vizColocDotplot(dat_filtered, zsig.thresh = zsig, zscore.limit = 2*zsig, reorder = TRUE, dot.sizes = c(2, 10)) +
+vizColocDotplot(dat_filtered, zsig.thresh = zsig, zscore.limit = 2*zsig, reorder = TRUE, dot.sizes = c(3, 15)) +
   coord_fixed() +
-  theme(axis.text.x = element_text(angle = 35, h = 0))
-ggsave(filename = here("running_code", "plots", dataset_name, paste0(dataset_name, "_summary_unlabeled_removed.pdf")), dpi = 300)
+  theme(
+    axis.text.x = element_text(angle = 90, h = 0),
+    legend.position = "bottom"
+  )
+ggsave(filename = here("running_code", "plots", dataset_name, paste0(dataset_name, "_summary_unlabeled_removed.pdf")), width = 12, height = 12, dpi = 300)
 
 ## Figure x (colocalizations at single-cell)
 
 # manually identified using a CRAWDAD summary plot (reordered, unlabeled cells removed)
+# niches <- list(
+#   factor(c("DCIS_1", "DCIS_2", "Myoepi_ACTA2+", "Myoepi_KRT15+"), levels = levels(ct_labels)),
+#   factor(c("DCIS_1", "DCIS_2", "Myoepi_ACTA2+", "Myoepi_KRT15+", "Invasive_Tumor", "Prolif_Invasive_Tumor"), levels = levels(ct_labels)),
+#   factor(c("Invasive_Tumor", "Prolif_Invasive_Tumor"), levels = levels(ct_labels)),
+#   factor(c("B_Cells", "CD4+_T_Cells", "CD8+_T_Cells", "Endothelial", "IRF7+_DCs", "LAMP3+_DCs", "Macrophages_1", "Macrophages_2", "Mast_Cells", "Perivascular-Like", "Stromal", "Stromal_&_T_Cell_Hybrid", "T_Cell_&_Tumor_Hybrid"), levels = levels(ct_labels)),
+#   factor(c("B_Cells", "CD4+_T_Cells", "CD8+_T_Cells", "Endothelial", "IRF7+_DCs", "LAMP3+_DCs", "Macrophages_1", "Macrophages_2", "Mast_Cells", "Perivascular-Like", "Stromal"), levels = levels(ct_labels))
+# )
+
 niches <- list(
   factor(c("DCIS_1", "DCIS_2", "Myoepi_ACTA2+", "Myoepi_KRT15+"), levels = levels(ct_labels)),
-  factor(c("DCIS_1", "DCIS_2", "Myoepi_ACTA2+", "Myoepi_KRT15+", "Invasive_Tumor", "Prolif_Invasive_Tumor"), levels = levels(ct_labels)),
   factor(c("Invasive_Tumor", "Prolif_Invasive_Tumor"), levels = levels(ct_labels)),
-  factor(c("B_Cells", "CD4+_T_Cells", "CD8+_T_Cells", "Endothelial", "IRF7+_DCs", "LAMP3+_DCs", "Macrophages_1", "Macrophages_2", "Mast_Cells", "Perivascular-Like", "Stromal", "Stromal_&_T_Cell_Hybrid", "T_Cell_&_Tumor_Hybrid"), levels = levels(ct_labels)),
-  factor(c("B_Cells", "CD4+_T_Cells", "CD8+_T_Cells", "Endothelial", "IRF7+_DCs", "LAMP3+_DCs", "Macrophages_1", "Macrophages_2", "Mast_Cells", "Perivascular-Like", "Stromal"), levels = levels(ct_labels))
+  factor(c("B_Cells", "CD4+_T_Cells", "CD8+_T_Cells", "Endothelial", "IRF7+_DCs", "LAMP3+_DCs", "Macrophages_1", "Macrophages_2", "Mast_Cells", "Perivascular-Like", "Stromal", "Stromal_&_T_Cell_Hybrid", "T_Cell_&_Tumor_Hybrid"), levels = levels(ct_labels))
 )
 
 for (celltypes in niches) {
@@ -123,14 +134,14 @@ for (celltypes in niches) {
   df_sub <- data.frame(spatialCoords(spe_sub), celltype = colData(spe_sub)$celltype)
   ggplot(df, aes(x = x, y = y)) +
     coord_fixed() +
-    rasterise(geom_point(color = "lightgray", size = 0.5, stroke = 0), dpi = 300) +
+    rasterise(geom_point(color = "lightgray", size = 0.3, stroke = 0), dpi = 300) +
     rasterise(geom_point(data = df_sub, aes(x = x, y = y, col = celltype), size = 0.5, stroke = 0), dpi = 300) +
-    scale_color_manual(values = col_ct[celltypes]) +
+    scale_color_manual(values = col_ct[celltypes], name = "cell types") +
     guides(col = guide_legend(override.aes = list(size = 3))) +
-    labs(title = paste0("Cell types: ", paste(celltypes, collapse = ", ")),
-         x = "x (um)",
-         y = "y (um)",
-         col = "Cell types") +
+    # labs(title = paste0("Cell types: ", paste(celltypes, collapse = ", ")),
+    #      x = "x (um)",
+    #      y = "y (um)",
+    #      col = "cell types") +
     theme_bw() +
     theme(
       panel.grid = element_blank(),
