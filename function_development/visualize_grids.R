dbdde2d9640c09ce115f52fbf512af4aecc99a51
Rafael dos Santos Peixoto <rafaeldossantospeixoto@gmail.com>
2024-03-08 17:10:26 -0500

    update viz grids

--- function_development/visualize_grids.R
+++ function_development/visualize_grids.R
@@ -1,3 +1,6 @@
+
+# Create function ---------------------------------------------------------
+
 #' Visualize grids and clusters
 #' 
 #' @description Uses the cells sf object and size of grid to visualize the grids 
@@ -5,6 +8,7 @@
 #' 
 #' @param cells sf object; spatial (x and y) coordinates and celltypes column
 #' @param gridSize numeric; size of the grid to plot
+#' @param square if false, make hexagonal grid (default TRUE)
 #' @param ofInterest character vector; a vector of specific clusters to visualize
 #' @param pointSize numeric; size of points
 #' @param alpha numeric; transparency of points
@@ -19,16 +23,42 @@
 #' }
 #' 
 #' @export
-vizGrids <- function(cells, gridSize, ofInterest = NULL,
-                     pointSize = 1, alpha = 0.5){
-  ## shuffling grid
-  grid <- sf::st_make_grid(cells, cellsize = gridSize)
+vizGrids <- function(cells, scale, shuffledList = NULL,
+                     totalPermutations = NULL, permutation = 1, 
+                     square = square,
+                     ofInterest = NULL, pointSize = 1, alpha = 0.5){
+
+  ## the total number of permutations is used to calculate the offset, so if the
+  ## user asks for a different permutation, but does not provide it, the code
+  ## will not work
+  if ((permutation != 1) & (is.null(totalPermutations))){
+    stop('provide the total number of permutations')
+  }
+  
+  ## create grids
+  if (permutation == 1) {
+    ## for no permutations 
+    ## create grid with no offset
+    grid <- sf::st_make_grid(cells, cellsize = scale, square = square)
+  } else {
+    ## for permutations 
+    ## calculate offset
+    offset <- -seq(from = 0, to = scale, by = scale/totalPermutations)[permutation]
+    ## get bounding box with min and max coordinates
+    bbox <- sf::st_bbox(cells$geometry)
+    ## create grid with offset
+    grid <- sf::st_make_grid(cells, cellsize = scale, 
+                             offset = c(bbox[['xmin']] + offset, 
+                                        bbox[['ymin']] + offset),
+                             square = square)
+  }
+  
+  
   ## get the coordinates of the centers of the grid tiles to add the tile IDs
   grid_coords_centroids <- as.data.frame(sf::st_coordinates(sf::st_centroid(grid)))
   grid_coords_centroids$name <- as.character(rownames(grid_coords_centroids))
   ## create plot
-  crawdad::vizClusters(cells = cells, ofInterest, 
-                                 pointSize, alpha) + 
+  crawdad::vizClusters(cells = cells, ofInterest, pointSize, alpha) + 
     ## add in the grid information on top of the plot
     ggplot2::geom_sf(data = grid, fill = NA) +
     ggplot2::geom_text(data = grid_coords_centroids, 
@@ -52,5 +82,20 @@ data('pkhl')
 ## convert dataframe to spatial points (SP)
 cells <- crawdad::toSF(pos = pkhl[,c("x", "y")], celltypes = pkhl$celltypes)
 ## plot
-vizGrids(cells, gridSize = 500, pointSize = .1)
+vizGrids(cells, scale = 500, pointSize = .1)
 vizClusters(cells, pointSize = .1)
+
+
+
+# Apply -------------------------------------------------------------------
+
+library(crawdad)
+library(tidyverse)
+ncores <- 7
+
+data(sim)
+sim <- crawdad:::toSF(pos = sim[,c("x", "y")],
+                      celltypes = sim$celltypes)
+dat <- readRDS('running_code/processed_data/dat_sim_50.RDS')
+
+vizGrids(sim, 300)
