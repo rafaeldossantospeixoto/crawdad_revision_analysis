31c708afc74b71036cc05ce56c54012259a0a995
Rafael dos Santos Peixoto <rafaeldossantospeixoto@gmail.com>
2024-03-11 17:36:38 -0400

    run nullsim

---
+++ running_code/ripleys/nullsim_ripleys.R
@@ -0,0 +1,43 @@
+library(crawdad)
+library(tidyverse)
+library(spatstat)
+
+nullsim <- read.csv(file = 'simulating_data/null_sim/df_nullsim_s0.csv', 
+                row.names = 1)
+head(nullsim)
+
+
+# Homogeneous -------------------------------------------------------------
+
+## run ripley's L
+## need to convert to these point process objects
+ppo  <- ppp(x=nullsim$x, y=nullsim$y, 
+            window = owin(range(nullsim$x), range(nullsim$y)), 
+            marks=factor(nullsim$celltypes))
+## run on all
+reference_ct <- 'A'
+## fixed scales
+radii <- seq(0, 500, by=5)
+rkc <- do.call(rbind, lapply(levels(ppo$marks), function(x) {
+  test <- Kcross(ppo, reference_ct, x, r = radii)
+  df_test <- data.frame(reference = reference_ct,
+                        neighbor = x,
+                        radius = test$r)
+  ## isotropic-corrected minus theoretical
+  df_test$score <- test$iso - test$theo
+  return(df_test)
+}))
+
+rkc %>%  
+  ggplot() +
+  geom_line(aes(x=radius, y=score, color=neighbor)) +
+  ggplot2::geom_hline(yintercept = 0, color = "black", size = 0.3, linetype = "solid") +
+  ggplot2::theme_classic() +
+  ggplot2::scale_color_manual(values = rainbow(length(unique(rkc$neighbor)))) +
+  labs(y = "isotropic minus theoretical score")
+
+df <- rkc
+df$permutation <- 1
+saveRDS(df, "running_code/processed_data/dat_nullsim_ripleys.RDS")
+
+
--- running_code/run_nullsim.R
+++ running_code/run_nullsim.R
@@ -5,6 +5,83 @@ library(crawdad)
 library(tidyverse)
 ncores <- 7
 
-dfs <- readRDS('simulating_data/null_sim/cells_nullsim.RDS')
+cells <- readRDS('simulating_data/null_sim/cells_nullsim_s0.RDS')
 
-vizClusters(dfs[[1]])
+vizClusters(cells)
+
+## reduced to 500
+scales <- seq(100, 500, by=50)
+
+## generate background
+shuffle.list <- crawdad:::makeShuffledCells(cells,
+                                            scales = scales,
+                                            perms = 10,
+                                            ncores = ncores,
+                                            seed = 1,
+                                            verbose = TRUE)
+
+## find trends, dist 50
+results_50 <- crawdad::findTrends(cells,
+                                  dist = 50,
+                                  shuffle.list = shuffle.list,
+                                  ncores = ncores,
+                                  verbose = TRUE,
+                                  returnMeans = FALSE)
+dat_50 <- crawdad::meltResultsList(results_50, withPerms = T)
+saveRDS(dat_50, 'running_code/processed_data/dat_nullsim_50.RDS')
+
+## multiple-test correction
+zsig <- correctZBonferroni(dat_50)
+
+## viz 50
+vizColocDotplot(dat_50, zscore.limit = zsig*2)
+vizColocDotplot(dat_50, zsig.thresh = zsig, zscore.limit = zsig*2)
+
+
+
+# Paper figures -----------------------------------------------------------
+
+dat_50 <- readRDS('running_code/processed_data/dat_nullsim_50.RDS')
+
+## error because no dot with significance
+zsig <- correctZBonferroni(dat_50)
+vizColocDotplot(dat_50, reorder = TRUE, zsig.thresh = zsig, 
+                zscore.limit = zsig*2, 
+                dot.sizes = c(2, 14)) +
+  theme(legend.position='right',
+        axis.text.x = element_text(angle = 45, h = 0))
+
+
+
+## Dotplot -----------------------------------------------------------------
+
+p <- vizColocDotplot(dat_50, reorder = F, 
+                     zscore.limit = zsig*2,
+                     dot.sizes = c(5, 20)) +
+  theme(legend.position='bottom',
+        axis.text.x = element_text(vjust = 1))
+p
+pdf('running_code/paper_figures/nullsim_dotplot_crawdad.pdf',
+    height = 4, width = 5)
+p
+dev.off()
+
+
+
+## Trends ------------------------------------------------------------------
+
+## will plot using same design for squidpy and ripleys
+ref_cts <- c(rep('A', 4), LETTERS[2:4])
+neigh_cts <- c(LETTERS[1:4], rep('A', 3))
+for (i in 1:length(ref_cts)) {
+  p <- dat_50 %>% 
+    filter(reference == ref_cts[i]) %>% 
+    filter(neighbor == neigh_cts[i]) %>% 
+    vizTrends(lines = TRUE, withPerms = TRUE, sig.thresh = zsig)
+  print(p)
+  # pdf(sprintf('running_code/paper_figures/nullsim/trend_ref%s_neigh%s_crawdad.pdf',
+  #             ref_cts[i], neigh_cts[i]),
+  #     height = 4, width = 5)
+  # print(p)
+  # dev.off()
+}
