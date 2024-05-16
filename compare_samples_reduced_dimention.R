a6729ce3eb87d465bd4d8e07cd5a6092ed1064ac
Rafael dos Santos Peixoto <rafaeldossantospeixoto@gmail.com>
2024-05-16 17:30:53 -0400

    run comparison analysis for auc on reduced dimention space

---
+++ compare_samples_reduced_dimention.R
@@ -0,0 +1,91 @@
+library(crawdad)
+library(tidyverse)
+
+
+# Read data ---------------------------------------------------------------
+
+## read auc data
+auc <- readRDS('running_code/outputs/merfish_mouseBrain_diff_auc_dist_50.RDS')
+
+## check for nans
+auc[is.na(auc$auc),]
+## most are Medium Spiny Neurons and s1r3
+
+## viz some nans
+readRDS('running_code/outputs/merfish_mouseBrain_s1_r3_findTrends_ct_cleaned_dist_50.RDS') %>% 
+  crawdad::meltResultsList(withPerms = TRUE) %>% 
+  filter(reference == 'Ependymal Cells',
+         neighbor == 'Medium Spiny Neurons') %>% 
+  vizTrends()
+readRDS('running_code/outputs/merfish_mouseBrain_s1_r3_findTrends_ct_cleaned_dist_50.RDS') %>% 
+  crawdad::meltResultsList(withPerms = TRUE) %>% 
+  filter(reference == 'Medium Spiny Neurons',
+         neighbor == 'Oligodendrocytes') %>% 
+  vizTrends()
+## there are nan Z-scores for some of the scales, so the auc is nan
+
+
+# Substitute NaNs for 0 ---------------------------------------------------
+
+## I will substitute the NaNs for 0, just to try
+## read auc data
+auc <- readRDS('running_code/outputs/merfish_mouseBrain_diff_auc_dist_50.RDS')
+auc <- auc %>% 
+  mutate(sample = paste0('s', slice, 'r', replicate), 
+         pair = paste0(paste0(reference, ' - ', neighbor))) %>% 
+  select(c(pair, sample, auc)) %>% 
+  replace_na(list(auc = 0))
+auc_mtx <- auc %>% 
+  pivot_wider(names_from = sample, values_from = auc) %>% 
+  select(!pair) %>% 
+  as.matrix() %>% 
+  t()
+## there are missing values in the matrix too because cell type pairs are missing
+auc_mtx[is.na(auc_mtx)] <- 0
+
+## calculate pca
+pca <- prcomp(auc_mtx)
+pcs <- pca$x[, 1:2] %>% 
+  as.data.frame() %>% 
+  mutate(sample = rownames(pca$x)) %>% 
+  mutate(slice = sapply(rownames(pca$x), 
+                        FUN = function(x) str_split(x, 'r')[[1]][1]))
+
+## plot
+pcs %>% 
+  ggplot() + 
+  geom_point(aes(x = PC1, y = PC2, color = slice)) + 
+  scale_color_manual(values = rainbow(3))
+## it worked
+
+
+
+# Drop pairs with NaNs ---------------------------------------------------
+
+## read auc data
+auc <- readRDS('running_code/outputs/merfish_mouseBrain_diff_auc_dist_50.RDS')
+auc <- auc %>% 
+  mutate(sample = paste0('s', slice, 'r', replicate), 
+         pair = paste0(paste0(reference, ' - ', neighbor))) %>% 
+  select(c(pair, sample, auc))
+auc_mtx <- auc %>% 
+  pivot_wider(names_from = sample, values_from = auc) %>% 
+  select(!pair) %>% 
+  drop_na() %>% ## drop nas
+  as.matrix() %>% 
+  t()
+
+## calculate pca
+pca <- prcomp(auc_mtx)
+pcs <- pca$x[, 1:2] %>% 
+  as.data.frame() %>% 
+  mutate(sample = rownames(pca$x)) %>% 
+  mutate(slice = sapply(rownames(pca$x), 
+                        FUN = function(x) str_split(x, 'r')[[1]][1]))
+
+## plot
+pcs %>% 
+  ggplot() + 
+  geom_point(aes(x = PC1, y = PC2, color = slice)) + 
+  scale_color_manual(values = rainbow(3))
+## still works
