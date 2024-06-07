15d28521276ebc133b9fe14765b5bdb4ff55227a
Rafael dos Santos Peixoto <rafaeldossantospeixoto@gmail.com>
2024-06-06 23:18:28 -0400

    update fsld spleen figures

--- running_code/run_spleen.R
+++ running_code/run_spleen.R
@@ -493,18 +493,18 @@ binomMat <- readRDS('running_code/processed_data/spleen/binomMat_fsld_50.RDS')
 # saveRDS(subset.list, file = 'running_code/processed_data/spleen/subsetlist_fsld_50.RDS')
 subset.list <- readRDS('running_code/processed_data/spleen/subsetlist_fsld_50.RDS')
 
-##
-results.subsets <- crawdad::findTrends(cells,
-                                       dist = 50,
-                                       shuffle.list = shuffle.list,
-                                       subset.list = subset.list,
-                                       ncores = ncores,
-                                       verbose = TRUE,
-                                       returnMeans = FALSE)
-## Time was 715.76 mins
-## subsets
-dats <- crawdad::meltResultsList(results.subsets, withPerms = TRUE)
-saveRDS(dats, file = 'running_code/processed_data/spleen/dats_fsld_50.RDS')
+# ##
+# results.subsets <- crawdad::findTrends(cells,
+#                                        dist = 50,
+#                                        shuffle.list = shuffle.list,
+#                                        subset.list = subset.list,
+#                                        ncores = ncores,
+#                                        verbose = TRUE,
+#                                        returnMeans = FALSE)
+# ## Time was 715.76 mins
+# ## subsets
+# dats <- crawdad::meltResultsList(results.subsets, withPerms = TRUE)
+# saveRDS(dats, file = 'running_code/processed_data/spleen/dats_fsld_50.RDS')
 dats <- readRDS('running_code/processed_data/spleen/dats_fsld_50.RDS')
 
 ## Multiple-test correction
@@ -521,23 +521,10 @@ ct_order <- readRDS('running_code/processed_data/ct_order_spleen.RDS')
 ## Spatial plot
 all_cts <- unique(cells$celltypes)
 interest_cts <- sort(as.character(all_cts[all_cts != 'indistinct']))
-ct_colors <- c('Sinusoidal cells' = '#FF0080',
-               'Myeloid cells' = '#0000FF',
-               'Neutrophils/Monocytes' = '#8000FF',
-               'Blood endothelial' = '#FF8000',
-               'CD8 Memory T cells' = '#80FF00',
-               'Macrophages' = '#0080FF',
-               'Fol B cells' = '#00FF00',
-               'B cells, red pulp' = '#FF0000',
-               'Ki67 proliferating' = '#00FFFF',
-               # 'indistinct' = '#00FF80',
-               'CD4 Memory T cells' = '#FFFF00',
-               'Podoplanin' = '#FF00FF')
-saveRDS(ct_colors, 'running_code/processed_data/colors_spleen.RDS')
+ct_colors <- readRDS('running_code/processed_data/colors_spleen.RDS')
 
 p <- vizClusters(cells, ofInterest = interest_cts, alpha = 1, pointSize = .01) +
   scale_color_manual(values = ct_colors, na.value = '#00FF80') +
-  coord_fixed() + 
   theme_void()
 p
 pdf('running_code/paper_figures/spleen/fsld_spatial_plot.pdf',
@@ -555,8 +542,16 @@ dat_filtered <- dat %>%
   filter(neighbor != 'indistinct') %>% 
   filter(reference != 'indistinct')
 
-p <- vizColocDotplot(dat_filtered, zsigThresh = zsig, zscoreLimit = zsig*2, 
+p <- vizColocDotplot(dat_filtered, zSigThresh = zsig, zScoreLimit = zsig*2, 
                      reorder = TRUE, dotSizes = c(2, 14)) +
+  scale_color_gradient2(low = "blue", mid = "white", high = "red",
+                        midpoint = 0,
+                        breaks = c(-zsig*2, 0, zsig*2),
+                        limits=c(-zsig*2, zsig*2)) + 
+  ggplot2::scale_radius(trans = 'reverse',
+                        breaks = c(1750, 1000, 500, 100),
+                        limits = c(1750, 100),
+                        range = c(2, 14)) +
   scale_x_discrete(limits = ct_order, position = 'top') +
   scale_y_discrete(limits = ct_order, position = 'right') +
   coord_fixed() + 
@@ -576,10 +571,7 @@ dev.off()
 ct_ngb <- 'CD4 Memory T cells'
 ct_ref <- 'Fol B cells'
 ct_subset <- paste(ct_ngb, 'near', ct_ref, sep = '_')
-## selected CD4 Color '#FFFF00' and got 30% shades above and below
-## https://maketintsandshades.com/#FFFF00
-## the other color is the Fol B
-colors_subset <- c('#b3b300', '#ffff4d', '#00FF00')
+colors_subset <- c('#b3005a', '#ff4da6', '#00FF80')
 
 idx_ref_all <- as.character(which(cells$celltypes == ct_ref))
 idx_ngb_all <- as.character(which(cells$celltypes == ct_ngb))
@@ -623,7 +615,6 @@ cells_subset <- cells %>%
 crawdad::vizClusters(cells = cells_subset)
 p <- vizClusters(cells_subset, alpha = 1, pointSize = .01) +
   scale_color_manual(values = colors_subset, na.value = '#E6E6E6') +
-  coord_fixed() + 
   theme_void()
 p
 pdf('running_code/paper_figures/spleen/fsld_subset_spatial_plot.pdf',
