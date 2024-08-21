789de6e439331fcc9d4868d58e4ba1188d9d640f
Rafael dos Santos Peixoto <rafaeldossantospeixoto@gmail.com>
2024-08-21 16:08:24 -0400

    update vizShuffledGrids function

--- function_development/viz_shuffled_labels.R
+++ function_development/viz_shuffled_labels.R
@@ -1,13 +1,16 @@
 
-#' Visualize grids and clusters
+#' Visualize grids and clusters after shuffling
 #' 
 #' @description Uses the cells sf object and size of grid to visualize the grids 
-#' and clusters.
+#' and the shuffled null background.
 #' 
 #' @param cells sf object; spatial (x and y) coordinates and celltypes column
 #' @param shuffledList list; cell type labels shuffled at different scales 
 #' (output from makeShuffledCells())
-#' @param gridSize numeric; size of the grid to plot
+#' @param scale numeric; size of the scale to plot
+#' @param permutation numeric; the number of the permutation of interest.
+#' @param square boolean; if true, create a squared grid, if false, make 
+#' hexagonal grid (default TRUE)
 #' @param ofInterest character vector; a vector of specific clusters to visualize
 #' @param pointSize numeric; size of points
 #' @param alpha numeric; transparency of points
@@ -22,18 +25,35 @@
 #' }
 #' 
 #' @export
-vizShuffledLabels <- function(cells, shuffledList, gridSize, permutation = 1, 
-                              ofInterest = NULL,
-                              pointSize = 1, alpha = 0.5){
+vizShuffledGrids <- function(cells, shuffledList, scale, 
+                             permutation = 1, 
+                             square = TRUE,
+                             ofInterest = NULL, pointSize = 1, alpha = 0.5){
   
-  shuffled_labels <- shuffledList[[as.character(gridSize)]][[as.character(permutation)]]
+  ## define colors to be consistent with the vizCluster function
+  cluster_cols <- rainbow(n = length(unique(cells$celltypes)))
+  names(cluster_cols) <- unique(cells$celltypes)
+  cluster_cols['other'] <- '#E6E6E6'
+  
+  ## assing the shuffled labels to the celltypes column
+  shuffled_labels <- shuffledList[[as.character(scale)]][[as.character(permutation)]]
   cells$celltypes <- as.factor(shuffled_labels)
+  ## determine the total number of permutations to perform the offsetting
+  totalPermutations <- length(shuffledList[[1]])
   
-  vizGrids(cells, gridSize, ofInterest, pointSize, alpha)
+  ## use vizGrids to plot
+  vizGrids(cells = cells, scale = scale, 
+           permutation = permutation, totalPermutations = totalPermutations,
+           square = square,
+           ofInterest = ofInterest, pointSize = pointSize, alpha = alpha) +
+    ## NA to gray
+    ggplot2::scale_color_manual(values = cluster_cols, na.value = "#E6E6E6")
   
 }
 
 
+
+# Test --------------------------------------------------------------------
 
 library(crawdad)
 
@@ -43,10 +63,12 @@ cells <- crawdad:::toSF(pos = sim[,c("x", "y")],
                         celltypes = sim$celltypes)
 ## generate background
 shuffle.list <- crawdad::makeShuffledCells(cells,
-                                           scales = seq(100, 1000, by=50),
+                                           scales = seq(250, 500, by=50),
                                            perms = 3,
                                            ncores = 5,
                                            seed = 1,
                                            verbose = TRUE)
 
-vizShuffledLabels(cells, shuffle.list, 300, 1)
+vizGrids(cells, scale =  300, permutation = 2, totalPermutations = 3)
+vizShuffledGrids(cells = cells, shuffledList = shuffle.list, scale = 300, 
+                  permutation = 2)
